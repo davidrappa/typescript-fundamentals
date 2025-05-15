cf32795b0092a8b6d5eb6e87ca454516b01e58d5
David Rappa <davidrappa@outlook.com>
2025-05-15 11:04:17 -0300

    feat: primitives and array class

---
+++ primitives.ts
@@ -0,0 +1,13 @@
+/**
+ * * Primitives
+ * In TypeScript (and JavaScript), primitives are the most basic data types that are not objects and have no methods.
+ * They represent single values.
+ */
+
+let name: string = "Lucas"; //string
+
+let age: number = 28; // number
+
+let isADeveloper: boolean = true; //boolean
+
+const array: number[] = [3, 4, 555, 3]; // array with numbers
