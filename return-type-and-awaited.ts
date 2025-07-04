e65f71b8bce68532c3cd54a349a4d65a9c7fb2ec
David <davidrappa@outlook.com>
2025-07-04 10:22:10 -0300

    feat: return type and awaited class

---
+++ return-type-and-awaited.ts
@@ -0,0 +1,22 @@
+import { User } from "./utility-models";
+
+/**
+ * Return Type
+ * Extract the return type of a function type.
+ */
+async function sendEmail(
+  user: Pick<User, "email" | "firstName">,
+  message: string
+): Promise<string> {
+  return message;
+}
+
+type SendEmailFn = typeof sendEmail;
+
+type Return = Awaited<ReturnType<SendEmailFn>>;
+
+type T1 = Promise<Promise<string>>;
+
+type T2 = Awaited<T1>;
+
+type T3 = Awaited<boolean | Promise<string>>;
